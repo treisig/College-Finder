b819002ebf54450775e826cb5c9f9b82c60c0ea5
Abigail Wingo <awingo@Abigails-MacBook-Pro-3.local>
2020-10-17 17:24:17 -0400

    sign up initial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
-import Login from "./components/authentication/Login.js";
+// import Login from "./components/authentication/Login.js";
 import Signup from "./components/authentication/Signup.js";
-import SchoolView from "./components/school-view/SchoolView.js";
+// import SchoolView from "./components/school-view/SchoolView.js";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/schoolfinder" component={SchoolView} />
         </Switch>
-      </Router>
+      </Router> */}
+      <Signup />
     </div>
   );
 }
