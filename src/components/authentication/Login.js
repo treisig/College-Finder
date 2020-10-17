7f2c3df25ded4f3ac34ee0734ce948a7a3beecd3
Abigail Wingo <awingo@Abigails-MacBook-Pro-3.local>
2020-10-17 15:51:13 -0400

    initial login

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./Login.css";
 
 function Login(props) {
-  return <div className="loginDiv"></div>;
+  return <div>
+    <div className="header">
+      header
+    </div>
+    <div className="loginDiv">
+    
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email Address: </Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password: </Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+      Submit
+      </Button>
+    </Form>
+   
+  </div>;
+  </div>
 }
 
 export default Login;
