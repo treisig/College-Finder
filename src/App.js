dced3d82ef65bd0a477ac607fc34b81f26815346
tucker reisig <tuckerreisig@Tuckers-MacBook-Pro.local>
2020-10-17 18:28:05 -0400

    inital auth without axios

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import React from "react";
-// import Login from "./components/authentication/Login.js";
+import Login from "./components/authentication/Login.js";
 import Signup from "./components/authentication/Signup.js";
-// import SchoolView from "./components/school-view/SchoolView.js";
+import SchoolView from "./components/school-view/SchoolView.js";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 
 function App() {
   return (
     <div className="App">
-
       <Router>
         <nav>
           <ul>
             <li>
               <Link to="/"> Login</Link>
             </li>
-            {/* <li>
-              <Link to="/signup">Signup</Link>
-            </li> */}
+            /
             <li>
               <Link to="/schoolfinder">SchoolView</Link>
             </li>
           </ul>
         </nav>
-
-      {/* <Router>
         <Switch>
           <Route exact path="/" component={Login} />
-          {/* <Route exact path="/signup" component={Signup} /> */}
+          <Route exact path="/signup" component={Signup} />
           <Route exact path="/schoolfinder" component={SchoolView} />
         </Switch>
-      </Router> */}
-      <Signup />
+      </Router>
     </div>
   );
 }
--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -1,36 +1,56 @@
 import React from "react";
-import { Form, Button, Col} from "react-bootstrap";
+import { Form, Button, Col } from "react-bootstrap";
 import "./Signup.css";
+import Firebase from "../../Firebase/Firebase.js";
+import axios from "axios";
 
 function Signup(props) {
-    return <div>
-      <div className="header">
-        
-      </div>
+  const handleSignup = (event) => {
+    event.preventDefault();
+    const { email, password, confirm } = event.target.elements;
+    if (password.value !== confirm.value) {
+      alert("Please make sure your passwords match");
+      return;
+    }
+    Firebase.auth
+      .createUserWithEmailAndPassword(email.value, password.value)
+      .catch((err) => {
+        console.log("Something went wrong, please try again.");
+      });
+
+    // axios.post("/createAccount", {
+    //   uid: Firebase.auth.c,
+    // });
+  };
+
+  return (
+    <div>
+      <div className="header"></div>
       <div className="signupDiv">
-        <Form>
-            <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Email: </Form.Label>
-                    <Form.Control type="email" />
-                </Form.Group>
+        <Form onSubmit={handleSignup}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email: </Form.Label>
+              <Form.Control name="email" type="email" />
+            </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label>Password: </Form.Label>
-                    <Form.Control type="password" />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label>Confirm Password: </Form.Label>
-                    <Form.Control type="password" />
-                </Form.Group>
-            </Form.Row>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Password: </Form.Label>
+              <Form.Control name="password" type="password" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Confirm Password: </Form.Label>
+              <Form.Control name="confirm" type="password" />
+            </Form.Group>
+          </Form.Row>
 
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
         </Form>
       </div>
-  </div>
+    </div>
+  );
 }
 
 export default Signup;
